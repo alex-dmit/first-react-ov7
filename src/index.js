aa854f0f206b5aefa587bf8f67ec5a392ba849f5
Alexander Dmitriev <test@example.com>
2023-02-16 22:06:54 +0300

    First react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App'
+import { useState } from 'react';
+
+// const jsxElement = <h1>Hello world!</h1>
+
+// React component
+function HelloComponent(props) {
+  console.log('Render HelloComponent');
+  // const message = "what's up"
+  const [message, setMessage] = useState('')
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <button onClick={() => {
+        console.log(count);
+        setCount(count + 1)
+      }}>+</button>
+      <span>Count: {count}</span>
+      <br/>
+      <input type="text" value={message} onChange={(event) => {
+        setMessage(event.target.value)
+      }}/>
+      <h1>Hello, {props.name} {props.lastname}, {message ? message : "what's up"}</h1>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelloComponent name="John" lastname="Smith" />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
